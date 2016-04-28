d937e20ef431475203d5ce38aea75ccda8eb249d
vin-droid <vineet.12cs069@abes.ac.in>
2016-04-28 10:54:56 +0530

    Added pg and rails12factor gem for heroku

--- app/models/user.rb
+++ app/models/user.rb
@@ -54,7 +54,10 @@ end
 def friend_with?(user)
 end
 
-# user cant send if connected with that user
+
+
+
+# user cant send if already connected with that user
 def connected_with?(user)
      Friendship.where(
         friendable_id: user.id , friend_id: self.id).count>0||
@@ -87,18 +90,80 @@ def has_friendships
 end
 
 
+
+
 # user can send friend request only these user 
-def has_frienshipable_friends
-    @all_other_users = User.where.not(id: self.id)
-    friendshipable_friends = Array.new
-   #find users not connected with  you 
-    @all_other_users.each do |friendshipable_friend|
-      if !(Friendship.exists?(friendable_id: friendshipable_friend.id , friend_id: self.id)||
-         Friendship.exists?(friendable_id: self.id , friend_id: friendshipable_friend.id))
-        friendshipable_friends.push(friendshipable_friend)
-      end
-  end
-         return friendshipable_friends
-end
+        def has_frienshipable_friends
+                @all_other_users = User.where.not(id: self.id)
+                friendshipable_friends = Array.new
+
+                #find users not connected with  you 
+                @all_other_users.each do |friendshipable_friend|
+                    if !(Friendship.exists?(friendable_id: friendshipable_friend.id , friend_id: self.id)||
+                                     Friendship.exists?(friendable_id: self.id , friend_id: friendshipable_friend.id)) 
+
+                         friendshipable_friends.push(friendshipable_friend)
+                    end
+                end
+           return friendshipable_friends
+        end
 
 end
+
+=begin
+# @mac sends a friend request to @dee
+@mac.friend_request(@dee)
+
+# @dee can accept the friend request
+@dee.accept_request(@mac)
+
+# @dee can also decline the friend request
+@dee.decline_request(@mac)
+A friendship can also be removed:
+
+# @dee removes @mac from its friends
+@dee.remove_friend(@mac)=end
+
+=begin
+Type of friends
+
+There are four types of friends:
+
+requested_friends
+pending_friends
+blocked_friends
+friends
+They can be accessed using association.
+
+requested_friends
+
+Instances that sent friend request that has not been accepted.
+
+@mac.friend_request(@dee)
+
+@dee.requested_friends # => [@mac]
+pending_friends
+
+Instances that received but has not accepted the friend request.
+ 
+@mac.friend_request(@dee)
+
+@mac.pending_friends # => [@dee]
+blocked_friends
+
+Instances that are blocked from taking any friendship actions
+
+@dee.friend_request(@mac)
+@mac.block_friend(@dee)
+
+@mac.blocked_friends # => [@dee]
+friends
+
+Instances with accepted Friendship.
+
+@mac.friend_request(@dee)
+@dee.accept_request(@mac)
+
+@mac.friends # => [@dee]
+@dee.friends # => [@mac]=end
+
